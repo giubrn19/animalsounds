1467e862f98a1d60d9d104f6eb7e35f5004f0e55
Barini <u2273847@live.warwick.ac.uk>
2024-06-27 13:02:03 +0100

    c

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,20 @@
+#' Animal Sounds
+#' The sounds that animal makes
+#'
+#' @param animal (the name of the animal)
+#' @param sound (the sound of the animal)
+#'
+#' @return ("The ", animal, " says ", sound, "!")
+#' @export
+#'
+#' @examples
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
   paste0("The ", animal, " says ", sound, "!")
 }
+
+?animal_sounds
+?animal_sounds
+
+??animal_sounds
